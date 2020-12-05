5460171c6232830c78ea6b030ad9abe6d77224b9
masterhand83 <robinmiguelsdj@gmail.com>
2020-12-05 01:05:32 -0600

    inicializacion

---
+++ src/practica4/Practica4.java
@@ -0,0 +1,25 @@
+/*
+ * INSTITUTO POLITECNICO NACIONAL
+ * ESCUELA SUPERIOR DE COMPUTO
+ * Materia: [MATERIA]
+ * Grupo: [GRUPO]
+ * Profesor: [Profesor]
+ * Alumno(s): Santiago Apreza Robin Miguel
+ * [Uso y proposito de la clase]
+ */
+package practica4;
+
+/**
+ *
+ * @author Skynet
+ */
+public class Practica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
